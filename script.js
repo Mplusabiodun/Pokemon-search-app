4307722cc7a692d3800692939d3c1027ed11fc53
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-09-24 13:44:32 +0100

    script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // let inputtedCash = document.getElementById("cash");
 let changeDue = document.getElementById("change-due");
+let validPokémon = https://pokeapi-proxy.freecodecamp.rocks/api/pokemon
 const purchaseBtn = document.getElementById("purchase-btn");
 
 let displayPrice = document.querySelector(".price-screen");
@@ -8,8 +9,14 @@ displayPrice.textContent = `Price: ${price}`;
 purchaseBtn.addEventListener("click", (event) => event.preventDefault());
 purchaseBtn.addEventListener("click", checkCashRegister);
 
-// When ENTER KEY is pressed to continue
-// cash.addEventListener("keydown", (e) => {
-//   if (e.key === "Enter") {
-//   }
-// });
+const fetchData = async () => {
+  try {
+    const res = await fetch(validPokémon);
+    const data = await res.json();
+    // showLatestPosts(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchData();
