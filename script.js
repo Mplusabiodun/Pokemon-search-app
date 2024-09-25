9e87875f85cd957c6dafa6a12a676b9660c49cc7
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-09-25 12:54:06 +0100

    fecthing the data(API)

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-// let inputtedCash = document.getElementById("cash");
-let changeDue = document.getElementById("change-due");
-let validPokémon = https://pokeapi-proxy.freecodecamp.rocks/api/pokemon
-const purchaseBtn = document.getElementById("purchase-btn");
+let searchInput = document.getElementById("search-input");
+let searchBtn = document.getElementById("search-button");
+let pokemonName = document.getElementById("pokemon-name");
+let pokemonId = document.getElementById("pokemon-id");
+let pokemonWeight = document.getElementById("weight");
+let pokemonHeight = document.getElementById("height");
+let pokemonImage = document.getElementById("sprite-container");
+let pokemonType = document.getElementById("types");
+let validPokémon = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
 
-let displayPrice = document.querySelector(".price-screen");
-displayPrice.textContent = `Price: ${price}`;
-
-purchaseBtn.addEventListener("click", (event) => event.preventDefault());
-purchaseBtn.addEventListener("click", checkCashRegister);
+searchBtn.addEventListener("click", (event) => event.preventDefault());
+// searchBtn.addEventListener("click", checkCashRegister);
 
 const fetchData = async () => {
   try {
     const res = await fetch(validPokémon);
     const data = await res.json();
+    console.log(data);
     // showLatestPosts(data);
   } catch (err) {
     console.log(err);
