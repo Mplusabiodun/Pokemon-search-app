40f6248fd32f938bbcc5567e86c220ef0ec77fa6
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-09-26 16:57:04 +0100

    fetching valid pokemon - script

--- script.js
+++ script.js
@@ -6,20 +6,67 @@ let pokemonWeight = document.getElementById("weight");
 let pokemonHeight = document.getElementById("height");
 let pokemonImage = document.getElementById("sprite-container");
 let pokemonType = document.getElementById("types");
-let validPokémon = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+let validPokemon = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+let pokemanData;
+let individualPokemanData;
 
-searchBtn.addEventListener("click", (event) => event.preventDefault());
-// searchBtn.addEventListener("click", checkCashRegister);
-
-const fetchData = async () => {
+const fetchValidPokemon = async () => {
   try {
-    const res = await fetch(validPokémon);
+    const res = await fetch(validPokemon);
     const data = await res.json();
+    // searchPokemon(data);
+    pokemanData = data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+fetchValidPokemon();
+
+const searchPokemon = async (event) => {
+  event.preventDefault();
+  let pokemanValidName = searchInput.value.toLowerCase();
+
+  const { results } = pokemanData;
+  const pokemon = results?.find((result) => {
+    return result.name === pokemanValidName;
+  });
+  if (pokemon) {
+    await fetchPokemon(pokemon.name || pokemon.id);
+  } else {
+  }
+
+  // const pokemonInfo = document.getElementsByClassName("top-container");
+  // pokemonInfo.innerHTML = `
+  //             <div class="name-and-id">
+  //               <span id="pokemon-name">${pokemon.name}</span>
+  //               <span id="pokemon-id">${pokemon.id}</span>
+  //             </div>
+  //             <div class="size">
+  //               <span id="weight"></span>
+  //               <span id="height"></span>
+  //             </div>
+  //             <div id="sprite-container" class="sprite-container"></div>
+  //             <div id="types"></div>
+  //           `;
+};
+
+// searchBtn.addEventListener("click", (event) => event.preventDefault());
+searchBtn.addEventListener("click", searchPokemon);
+
+const fetchPokemon = async (pokemanIdentifier) => {
+  try {
+    const res = await fetch(
+      `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemanIdentifier}`
+    );
+    const data = await res.json();
+    individualPokemanData = data;
     console.log(data);
-    // showLatestPosts(data);
   } catch (err) {
     console.log(err);
   }
 };
 
-fetchData();
+// if (pokemanName.trim() === "") {
+//   alert("Please enter a Pokémon name!");
+//   return;
+// }
